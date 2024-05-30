ffbced706b28ad4f9dce5d333b998ca41f8a7e4a
Alex <ya.js100@yandex.ru>
2024-05-30 15:33:21 +0300

    v1

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,9 +1,17 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Egg6 {
+    private Deth8 deth8;
+
+    @Autowired
+    public void setDeth8(Deth8 deth8) { this.deth8 = deth8; }
 
     @Override
     public String toString() {
-        return ", в яйце иголка " + "";
+        return ", в яйце иголка " + deth8.toString();
     }
 }
